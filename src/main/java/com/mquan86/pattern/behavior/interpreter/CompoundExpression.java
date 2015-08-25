dc773517494c98058f3c28ea2df9d67f18c0bf0f
Quan <mquan86@gmail.com>
2015-08-25 10:13:49 +1000

    Interpreter pattern

---
+++ src/main/java/com/mquan86/pattern/behavior/interpreter/CompoundExpression.java
@@ -0,0 +1,12 @@
+package com.mquan86.pattern.behavior.interpreter;
+
+/**
+ * @author Minh Quan Pham (mquan86@gmail.com)
+ *
+ */
+public class CompoundExpression implements Expression {
+
+    @Override
+    public void interpreter(Context context) {}
+
+}
---
+++ src/main/java/com/mquan86/pattern/behavior/interpreter/Expression.java
@@ -0,0 +1,9 @@
+package com.mquan86.pattern.behavior.interpreter;
+
+/**
+ * @author Minh Quan Pham (mquan86@gmail.com)
+ *
+ */
+public interface Expression {
+    public void interpreter(Context context);
+}
